73d198c6c63c4c44902a4cb7362095854a79904d
GBailey23 <96180369+TheGrinch1@users.noreply.github.com>
2023-01-21 23:50:55 -0800

    v 0.01 inventory system
    bare bones if/else statement and extremely barebones menu

---
+++ inventorySystem.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <stdexcept>
+
+
+
+void displayMenu();
+void addData();
+void deleteData();
+void printData();
+
+
+int main(){
+    int choice;
+    std::cout << "***********************************\n";
+    std::cout << "Welcome to the part inventory system\n";
+    std::cout << "***********************************\n";
+    std::cout << "Please enter a selection below \n";
+    std::cout << "***********************************\n";
+    std::cout << " 1.view current inventory\n 2.add part to list\n 3.remove part from list\n 4.exit menu\n";
+    std::cout << "***********************************\n";
+    std::cin >> choice;
+
+    if(){
+
+
+
+
+
+    }
+    else if(){
+
+
+
+    }
+    else if (){
+
+
+    }
+    else(){
+        std::cout << "Incorrect selection , please try again\n";
+    }
+
+
+
+
+
+
+
+
+
+    return 0;
+}
